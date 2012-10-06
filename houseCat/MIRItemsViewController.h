91f79fac3226f30e719ac793a60cdb5b0d438bbe
kenl <kenl97216@gmail.com>
2012-10-06 06:59:14 -0700

    Created initial storyboard.

---
+++ houseCat/MIRItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MIRitemsViewController.h
+//  houseCat
+//
+//  Created by kenl on 12/10/4.
+//  Copyright (c) 2012 kl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIRItemsViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ houseCat/MIRRoomsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MIRroomsViewController.h
+//  houseCat
+//
+//  Created by kenl on 12/10/4.
+//  Copyright (c) 2012 kl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIRRoomsViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
